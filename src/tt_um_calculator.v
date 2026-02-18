0cf73dc3862945a88446df564cec09dd8a26fa3b
Aravindhan-project <aravindhan15ra@gmail.com>
2026-02-18 19:12:57 +0530

    tt_um_calculator.v

--- src/tt_um_calculator.v
+++ src/tt_um_calculator.v
@@ -1,35 +1,24 @@
 module tt_um_calculator (
-    input  wire clk,
-    input  wire rst_n,
-    input  wire ena,
     input  wire [7:0] ui_in,
     output wire [7:0] uo_out,
     input  wire [7:0] uio_in,
     output wire [7:0] uio_out,
-    output wire [7:0] uio_oe
+    output wire [7:0] uio_oe,
+    input  wire ena,
+    input  wire clk,
+    input  wire rst_n
 );
-
-assign uio_out = 8'b0;
-assign uio_oe  = 8'b0;
 
 wire [3:0] A = ui_in[3:0];
 wire [3:0] B = ui_in[7:4];
 
-reg [7:0] result;
+wire [4:0] SUM = A + B;
 
-always @(posedge clk or negedge rst_n) begin
-    if (!rst_n)
-        result <= 0;
-    else begin
-        case(uio_in[1:0])
-            2'b00: result <= A + B;
-            2'b01: result <= A - B;
-            2'b10: result <= A * B;
-            2'b11: result <= A & B;
-        endcase
-    end
-end
+assign uo_out[3:0] = SUM[3:0];
+assign uo_out[4]   = SUM[4];
+assign uo_out[7:5] = 3'b000;
 
-assign uo_out = result;
+assign uio_out = 8'b00000000;
+assign uio_oe  = 8'b00000000;
 
 endmodule
