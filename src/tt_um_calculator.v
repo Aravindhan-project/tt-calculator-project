36ee2d92ec51214e71c9d792f8cd7bc4ee5eefa7
Aravindhan-project <aravindhan15ra@gmail.com>
2026-02-18 18:59:29 +0530

    tt_um_calculator.v

--- src/tt_um_calculator.v
+++ src/tt_um_calculator.v
@@ -1,12 +1,12 @@
 module tt_um_calculator (
-    input  wire [7:0] ui_in,    // switches
-    output wire [7:0] uo_out,   // leds
+    input  wire clk,
+    input  wire rst_n,
+    input  wire ena,
+    input  wire [7:0] ui_in,
+    output wire [7:0] uo_out,
     input  wire [7:0] uio_in,
     output wire [7:0] uio_out,
-    output wire [7:0] uio_oe,
-    input  wire ena,
-    input  wire clk,
-    input  wire rst_n
+    output wire [7:0] uio_oe
 );
 
 assign uio_out = 8'b0;
@@ -22,10 +22,10 @@ always @(posedge clk or negedge rst_n) begin
         result <= 0;
     else begin
         case(uio_in[1:0])
-            2'b00: result <= A + B; // ADD
-            2'b01: result <= A - B; // SUB
-            2'b10: result <= A * B; // MUL
-            2'b11: result <= A & B; // AND
+            2'b00: result <= A + B;
+            2'b01: result <= A - B;
+            2'b10: result <= A * B;
+            2'b11: result <= A & B;
         endcase
     end
 end
